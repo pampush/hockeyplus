631aed2ef500b3cef509c901685295f288391e11
pampush <32041268+pampush@users.noreply.github.com>
2017-10-10 18:19:08 +0300

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,78 @@
 var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  browserSync = require('browser-sync').create();
+    sass = require('gulp-sass'),
+    notify = require('gulp-notify'),
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    pug = require('gulp-pug');
+    chmod = require('gulp-chmod');  // ??
+
+gulp.task('clean', () => {
+  return del([
+    './build/**/*.*', './build/**'
+  ]);
+});
+
 // convertation
 gulp.task('sass', () => { /* signal async completion issue */
   return gulp.src('./dev/static/sass/main.sass')
-    .pipe(sass().on('error', sass.logError)) // convert command
-    .pipe(gulp.dest('./dev/static/css'))
-    //.pipe(browserSync.stream());
-    .pipe(browserSync.reload({
+    .pipe(sass()) // convert command
+    .on('error', notify.onError(function (error) {
+      return {
+        title: 'sass',
+        message: error.message
+      };
+    }))
+    .pipe(gulp.dest('./build/static/css/'))
+    .pipe(browserSync.stream());
+    /*.pipe(browserSync.reload({
       stream: true
-    }));
+    }));*/
 });
 
-/* test
-gulp.task('html', () => {
-    return gulp.src('./dev/*.html')
-    .pipe(browserSync.stream());
+gulp.task('pug', () => {
+  return gulp.src('./dev/pug/pages/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .on('error', notify.onError(function(error) {
+      return {
+        title: 'pug',
+        message: error.message
+      };
+    }))
+    .pipe(gulp.dest('./build'))
+    .on('end',browserSync.reload);
+    //.pipe(browserSync.stream()); what difference between?
+    /*.pipe(browserSync.reload({
+      stream: true
+    }));*/
+});
 
-});*/
+gulp.task('fonts', () => {
+  return gulp.src('./dev/static/fonts/**/*.*')
+    .pipe(gulp.dest('./build/static/fonts'))
+});
+
+gulp.task('img', () => {
+  return gulp.src('./dev/static/img/**/*.{png,jpg,gif,svg}')
+    .pipe(gulp.dest('./build/static/img'))
+});
 
 // page initialisation + html,img,fonts. need to clean via del plugin
-gulp.task('dev', gulp.series('sass'));
+gulp.task('dev', gulp.series('clean', gulp.parallel('sass','pug','fonts','img')));
 
-// watchers + fonts, img ...
+// watchers + fonts, ismg ...
 gulp.task('watch', () => {
+  gulp.watch('./dev/pug/**/*.pug', gulp.series('pug'));
   gulp.watch('./dev/static/sass/**/*.sass', gulp.series('sass'));
-  gulp.watch('./dev/*.html').on('change',browserSync.reload);
+  gulp.watch('./dev/static/img/*.*', gulp.series('img'));
+  //.on('change',browserSync.reload);
 });
 
 // server init
 gulp.task('serve', () => {
   browserSync.init({
-    server: './dev'
+    server: './build'
   });
 });
 
